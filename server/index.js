a1c9840053af965ec990827095a17ac2cb00363e
Houssem Hmidi <102802046+HmidiHoussem@users.noreply.github.com>
2022-12-03 18:44:22 +0100

    server mongo db

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const db = require("./config/db");
 
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3070;
 
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.listen(PORT, function () {
