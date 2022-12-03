8a6c62cc82bed1f0e3a112080168acb251da17b3
Houssem Hmidi <102802046+HmidiHoussem@users.noreply.github.com>
2022-12-03 18:10:09 +0100

    server files

--- server/index.js
+++ server/index.js
@@ -6,10 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3070;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/../client/public"));
-app.use("/movie", movieRoute);
-app.use("/user", userRourte);
+// app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
